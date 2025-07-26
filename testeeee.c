56b2115eb520972d0674174491ea985c407fbe94
antobriels <antobriels@gmail.com>
2025-07-26 22:06:47 +0000

    super trunfo novato

---
+++ testeeee.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+    char estado1[5], cidade1[50];
+   
+    printf("Digite a cidade da primeira carta: \n");
+    scanf(" %[^\n]", &cidade1);
+
+    printf("nome da cidade: %s", cidade1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
